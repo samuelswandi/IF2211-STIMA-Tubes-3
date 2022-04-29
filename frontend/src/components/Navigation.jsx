ea05b22b26b4ac9bc99609c8c7051a14543b2787
samuelswandi <samuel.swandi@gmail.com>
2022-04-29 21:32:55 +0700

    change navbar

--- frontend/src/components/Navigation.jsx
+++ frontend/src/components/Navigation.jsx
@@ -12,7 +12,7 @@ const Navigation = () => {
         <Nav>
           <Nav.Link href="/TambahPenyakit">Tambah Penyakit</Nav.Link>
           <Nav.Link href="/TesDNA">Tes DNA</Nav.Link>
-          <Nav.Link href="/RiwayatPenyakit">Riwayat Penyakit</Nav.Link>
+          <Nav.Link href="/RiwayatPenyakit">Cek Riwayat</Nav.Link>
           <Nav.Link href="/TentangKami">Tentang Kami</Nav.Link>
         </Nav>
       </Container>
